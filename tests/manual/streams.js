6683946c3855434b190bfec30155143389380f47
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-01-13 01:54:57 -0600

    Simplified test.

--- tests/manual/streams.js
+++ tests/manual/streams.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 const {promisify} = require('util');
 
 const gen = require('../../gen');
 
 const pipe = gen(
-  fs.createReadStream(path.resolve(__dirname, 'streams.js'), {encoding: 'utf8'}),
+  fs.createReadStream(__filename, {encoding: 'utf8'}),
   s => s.replace(/a/g, 'o'),
   s => s.replace(/m/g, 'w')
 );
