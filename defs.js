c54fa045ce8a549b08571269182f6b385c7f99d6
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-08-12 16:27:33 -0500

    Added getters, exported all symbols.

--- defs.js
+++ defs.js
@@ -5,12 +5,15 @@ const finalSymbol = Symbol.for('object-stream.final');
 const manySymbol = Symbol.for('object-stream.many');
 const flushSymbol = Symbol.for('object-stream.flush');
 
+const finalValue = value => ({[finalSymbol]: 1, value});
+const many = values => ({[manySymbol]: 1, values});
+
 const isFinalValue = o => o && o[finalSymbol] === 1;
 const isMany = o => o && o[manySymbol] === 1;
 const isFlushable = o => o && o[flushSymbol] === 1;
 
-const finalValue = value => ({[finalSymbol]: 1, value});
-const many = values => ({[manySymbol]: 1, values});
+const getFinalValue = value => value.value;
+const getManyValues = value => value.values;
 
 const flushable = (write, final = null) => {
   const fn = final ? value => (value === none ? final() : write(value)) : write;
@@ -20,4 +23,19 @@ const flushable = (write, final = null) => {
 
 class Stop extends Error {}
 
-module.exports = {none, stop, many, finalValue, flushable, Stop, isFinalValue, isMany, isFlushable};
+module.exports = {
+  none,
+  stop,
+  Stop,
+  finalSymbol,
+  manySymbol,
+  flushSymbol,
+  finalValue,
+  many,
+  flushable,
+  isFinalValue,
+  isMany,
+  isFlushable,
+  getFinalValue,
+  getManyValues
+};
